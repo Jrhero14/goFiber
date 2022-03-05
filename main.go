5a8b84c32ce0bd0439098bffe14e8c1a3800401e
JrHero14 <overload.emperor14@gmail.com>
2022-03-05 13:31:58 +0800

    Finish Develop

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"github.com/Jrhero14/goFiber/database"
+	"github.com/Jrhero14/goFiber/router"
+	"github.com/gofiber/fiber/v2"
+)
 
+func main() {
+	// Start a new fiber app
+	app := fiber.New()
+
+	// Connect to the Database
+	database.ConnectDB()
+
+	// Setup the router
+	router.SetupRoutes(app)
+
+	// Listen on PORT 3000
+	app.Listen(":3000")
 }
--- router/router.go
+++ router/router.go
@@ -1,1 +1,14 @@
 package router
+
+import (
+	noteRoutes "github.com/Jrhero14/goFiber/internal/routes/note"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func SetupRoutes(app *fiber.App) {
+	api := app.Group("/api", logger.New())
+
+	// Setup the Node Routes
+	noteRoutes.SetupNoteRoutes(api)
+}
